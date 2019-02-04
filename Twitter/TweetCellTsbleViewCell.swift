30586d64d1ea0d8c2ee34cc3621038b95307d157
Du Tran <maclab@ics.uci.edu>
2019-02-04 00:24:50 -0800

    built everything

---
+++ Twitter/TweetCellTsbleViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCellTsbleViewCell.swift
+//  Twitter
+//
+//  Created by Kay Lab on 2/3/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTsbleViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
